0b4cb8e2740ba176f4c7af764ce4e4975e3b254d
JadElKattar <jad.el.kattar@gmail.com>
2021-12-05 10:06:39 +0000

    fix file

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -1,30 +1,27 @@
 #include "hblk_crypto.h"
 
 /**
- * ec_sign - sign a set of bytes, using given private EC_KEY
- * @key: pointer to EC_KEY struct containing private key to perform the signing
- * @msg: pointer to characters to be signed
- * @msglen: len of msg
- * @sig: address to store signature
- *
- * Return: pointer to signature buffer on success, NULL on error
+ * ec_sign - signs a given set of bytes, using a given EC_KEY private key
+ * @key: points to the EC_KEY structure containing the private key
+ * to be used to perform the signature
+ * @msg: points to the characters to be signed
+ * @msglen: length of msg
+ * @sig: holds the address at which to store the signature
+ * Return: pointer to the signature buffer upon success, NULL on failure
  */
-uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen,
-		sig_t *sig)
+uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg,
+		size_t msglen, sig_t *sig)
 {
-	unsigned char md[SHA256_DIGEST_LENGTH];
+	unsigned int len;
 
-	if (!key || !msg || !sig)
+	if (key == NULL || msg == NULL || sig == NULL)
 		return (NULL);
-	if (!EC_KEY_check_key(key))
+
+	len = sig->len;
+	/*Sign the message*/
+	if (ECDSA_sign(0, msg, msglen, sig->sig, &len, (EC_KEY *)key) != 1)
 		return (NULL);
-	if (!SHA256(msg, msglen, md))
-		return (NULL);
-	sig->len = ECDSA_size(key);
-	if (!sig->len)
-		return (NULL);
-	if (!ECDSA_sign(EC_CURVE, md, SHA256_DIGEST_LENGTH, sig->sig,
-				(unsigned int *)&(sig->len), (EC_KEY *)key))
-		return (NULL);
+	sig->len = len;
+
 	return (sig->sig);
 }
