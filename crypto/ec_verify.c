793e01cd33f8f330066b779f3759c1b01564af55
JadElKattar <jad.el.kattar@gmail.com>
2021-12-05 10:12:27 +0000

    Fix file

--- crypto/ec_verify.c
+++ crypto/ec_verify.c
@@ -1,28 +1,21 @@
 #include "hblk_crypto.h"
 
 /**
- * ec_verify - verify the signature of a given set of bytes using public key
- * @key: pointer to EC_KEY struct containing public key
- * @msg: pointer to characters to verify signature of
- * @msglen: len of msg
- * @sig: pointer to signature to check
- *
- * Return: 1 if signature valid, 0 if not
+ * ec_verify - verifies the signature of a given set of bytes,
+ * using a given EC_KEY public key
+ * @key: points to the EC_KEY structure containing the public key
+ * to be used to verify the signature
+ * @msg: points to the characters to be signed
+ * @msglen: length of msg
+ * @sig: points to the signature to be checked
+ * Return: 1 if the signature is valid, and 0 otherwise
  */
-int ec_verify(EC_KEY const *key, uint8_t const *msg, size_t msglen,
-		sig_t const *sig)
+int ec_verify(EC_KEY const *key, uint8_t const *msg,
+		size_t msglen, sig_t const *sig)
 {
-	unsigned char dgst[SHA256_DIGEST_LENGTH];
-
-	if (!key || !msg || !sig)
+	if (key == NULL || msg == NULL || sig == NULL)
 		return (0);
-
-	if (!EC_KEY_check_key(key))
-		return (0);
-	if (!SHA256(msg, msglen, dgst))
-		return (0);
-	if (ECDSA_verify(0, dgst, SHA256_DIGEST_LENGTH, sig->sig, sig->len,
-				(EC_KEY *)key) != 1)
-		return (0);
-	return (1);
+	/*Verify the signature of a message*/
+	return (ECDSA_verify(0, msg, msglen, sig->sig, sig->len,
+				(EC_KEY *)key) == 1);
 }
